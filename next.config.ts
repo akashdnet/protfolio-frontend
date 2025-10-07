61480919507063f8db82ff09fd7778e0b6f783cc
akashdnet <khondokerakashmi@gmail.com>
2025-10-07 23:35:40 +0600

    issue 6

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['picsum.photos', 'images.unsplash.com', "res.cloudinary.com"],
-  },
-  experimental:{
-    serverActions: {
-      bodySizeLimit: "10mb"
-    }
-  }
+  /* config options here */
 };
 
 export default nextConfig;
